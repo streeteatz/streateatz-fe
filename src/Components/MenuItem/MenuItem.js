28e248cbee39ce47c3e23ac3aa9a9f62932dbeff
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-07-08 10:56:49 -0700

    feat- shows the bones added

---
+++ src/Components/MenuItem/MenuItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MenuItem = ({item}) => {
+ 
+    return (
+        <div>
+            <h3>Burgers</h3>
+            <span><p>CheeseBurger</p><p>$8.00</p></span>
+            <span><p>BaconBurger</p><p>$9.00</p></span>
+        </div>
+    )
+}
