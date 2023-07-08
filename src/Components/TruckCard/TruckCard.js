ddd0fa9f396b8ffa9f22be5f22e773ef767aca7b
Em Lindvall <122124478+emlindvall@users.noreply.github.com>
2023-07-08 15:01:42 -0500

    Start adding basic architecture, assets, and props for TruckCard component

--- src/Components/TruckCard/TruckCard.js
+++ src/Components/TruckCard/TruckCard.js
@@ -1,8 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import notFav from '../../assets/notFav.png'
+import Fav from '../../assets/fav.png'
+import notDownvoted from '../../assets/notDownvoted.png'
+import notUpvoted from '../../assets/notUpvoted.png'
+import downvoted from '../../assets/downvoted.png'
+import upvoted from '../../assets/upvoted.png'
 
-const TruckCard = () => {
+const TruckCard = ({ truck, addFav, remFav }) => {
   return(
-    <div></div>
+    <div>
+      <img className="notFav icon" alt="empty star icon" src={notFav} onClick={(event) => addFav(truck)}/>
+      <img className="fav icon" alt="favorited icon" src={Fav}/>
+      <img className="notDownvoted icon" alt="empty thumbs down icon" src={notDownvoted} />
+      <img className="notUpvoted icon" alt="empty thumbs up icon" src={notUpvoted} />
+      <img className="downvoted icon" alt="downvoted icon" src={downvoted} />
+      <img className="upvoted icon" alt="upvoted icon" src={upvoted} />
+      <p className="name">Name</p>
+      <p className="location">Location</p>
+      <p className="distance">Distance</p>
+      <p className="status">Status</p>
+      <p className="tags">Tags</p>
+      <p className="upvotes">Upvote</p>
+      <p className="downvotes">Downvote</p>
+      <div className="fake-image"></div>
+    </div>
   )
 }
 
