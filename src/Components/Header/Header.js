eb24b31ca27db002484479de27d088ac116e71f9
Devynne Marshall <devynnem@gmail.com>
2023-07-10 14:08:32 -0600

    feat:Add navlinks to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,13 +6,19 @@ import { Routes, Route, NavLink } from 'react-router-dom';
 const Header = ({ togView }) => {
   return (
     <div className="header-container">
-      <div className="logo-container">
-        <img src={logo} id="spin-hover" className="logo-img" alt="streeteatz-logo"/>
-        <p className="logo-name">street eatz</p>
-      </div>
+      <NavLink to="/" >
+        <div className="logo-container">
+          <img src={logo} id="spin-hover" className="logo-img" alt="streeteatz-logo" />
+          <p className="logo-name">street eatz</p>
+        </div>
+      </NavLink>
       <div className="toggle-container">
-        <button id="vendor" className="vendor-btn btn" onClick={(event) => togView(event.target.id)}>vendors</button>
-        <button id="customer" className="customer-btn selected-btn" onClick={(event) => togView(event.target.id)}>customers</button>
+        <NavLink to="/vendor-view" >
+          <button id="vendor" className="vendor-btn btn" onClick={(event) => togView(event.target.id)}>vendors</button>
+        </NavLink>
+        <NavLink to="/">
+          <button id="customer" className="customer-btn selected-btn" onClick={(event) => togView(event.target.id)}>customers</button>
+        </NavLink>
       </div>
     </div>
   )
