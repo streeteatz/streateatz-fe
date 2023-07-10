a2ba8cf566bd04b132a2d01d98fbb4e3e844659c
Devynne Marshall <devynnem@gmail.com>
2023-07-10 14:17:06 -0600

    feat:Add a navlink to each food truck to take them to their specific details

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -2,13 +2,14 @@ import React from 'react'
 import TruckCard from '../TruckCard/TruckCard'
 
 const Results = ({ vendors, addFav, remFav, favorites }) => {
-  const allVendors = vendors.map((truck, element) =>  {
+  const allVendors = vendors.map((truck) =>  {
     return(
       <TruckCard 
         truck={truck}
         addFav={addFav}
         remFav={remFav}
-        key={element} />
+        key={truck.id} 
+        />
     )
   })
 
