8c85ce5b42eeb82525ff4da64a5a82c7a9d80168
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-07-13 16:43:58 -0700

    feat-shows the menu rendered in with basic css as well as USD

--- src/Components/MenuItem/MenuItem.js
+++ src/Components/MenuItem/MenuItem.js
@@ -1,11 +1,22 @@
 import React from "react";
-
+import './MenuItem.css';
 export const MenuItem = ({ item }) => {
  console.log(item, 'in menu')
+
+ const formattedPrice = item.price.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+});
+let USDollar = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
     return (
-        <div>
-            <span><h3>{item.name}</h3><p>{item.price}</p></span>
-            <p>{item.description}</p>
+        <div className="menu-item">
+            <span className="menu-top"><h3>{item.name}</h3><p>{USDollar.format(item.price)}</p></span>
+            <div className="menu-bottom">
+                <p>{item.description}</p>
+            </div>
         </div>
     )
 }
