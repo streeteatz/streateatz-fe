4891b369c4ccdc01f74163cb0ce66157065cafd7
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-07-14 17:36:37 -0400

    Add testing for the hompage logo, search bar and search buttons

--- cypress/e2e/main_spec.cy.js
+++ cypress/e2e/main_spec.cy.js
@@ -9,3 +9,28 @@ beforeEach(() => {
 it("should display a title", () => {
   cy.contains('h2', 'hungry?')
 });
+
+it("should have a logo and display app name", () => {
+  cy.get('img').should('have.class', 'logo-img')
+  cy.get('p').should('have.class', 'logo-name')
+});
+
+it("should have a search bar, icon and search buttons", () => {
+  cy.get('input').should('have.class', 'search-field')
+  cy.get('button').should('have.class', 'searchQuerySubmit')
+  cy.get('button').contains('open now')
+  cy.get('button').contains('favorites')
+  cy.get('button').contains('clear')
+});
+
+it("should have a toggle button with vendors and customers", () => {
+  cy.contains('h2', 'hungry?')
+});
+
+it("should display a title", () => {
+  cy.contains('h2', 'hungry?')
+});
+
+it("should display a title", () => {
+  cy.contains('h2', 'hungry?')
+});
