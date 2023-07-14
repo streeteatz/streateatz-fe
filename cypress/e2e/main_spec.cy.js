b519f4f92c7e2c3322daa34973a0ed8367768bce
Devynne Marshall <devynnem@gmail.com>
2023-07-14 14:35:14 -0600

    feat:add first test to cypress main spec to make sure the test data works

--- cypress/e2e/main_spec.cy.js
+++ cypress/e2e/main_spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+beforeEach(() => {
+  cy.intercept("GET", "https://streeteatz-be-b15261620498.herokuapp.com/vendors/", {
+    statusCode: 200,
+    fixture: "truckData.json"
   })
-})
+  .visit("http://localhost:3000/")
+});
+
+it("should display a title", () => {
+  cy.contains('h2', 'hungry?')
+});
