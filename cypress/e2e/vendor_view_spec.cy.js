3a88ebce7d898ac6e5037efdd64150724e198172
Devynne Marshall <devynnem@gmail.com>
2023-07-14 16:26:07 -0600

    feat:add inital testing to vendor view and add in a menu item file

--- cypress/e2e/vendor_view_spec.cy.js
+++ cypress/e2e/vendor_view_spec.cy.js
@@ -1,5 +1,34 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Vendor Page', () => {
+
+  beforeEach(() => {
+    cy.intercept("GET", "https://streeteatz-be-b15261620498.herokuapp.com/vendors/", {
+      statusCode: 200,
+      fixture: "truckData.json"
+    });
+    cy.intercept("GET", "https://streeteatz-be-b15261620498.herokuapp.com/vendors/9", {
+      statusCode: 200,
+      fixture: "truckData.json"
+    })
+    cy.intercept("GET", "https://streeteatz-be-b15261620498.herokuapp.com/items/", {
+      statusCode: 200,
+      fixture: "truckData.json"
+    })
+    // cy.intercept("GET", "http://localhost:3001/socket.io/?EIO=4&transport=polling&t=ObMVm5g", {
+    //   statusCode: 200,
+    //   fixture: "truckData.json"
+    // })
+    cy.visit("http://localhost:3000/");
+    cy.wait(2000);
+    cy.get('.right-container')
+      .get('.toggle-container')
+      .get('button').first().click();
+  });
+
+  it('should be able to go to Vendor View from home page', () => {
+    cy.get('.status-container')
+      .get('.status-header')
+    cy.contains('welcome back!')
+  });
+
+
 })
