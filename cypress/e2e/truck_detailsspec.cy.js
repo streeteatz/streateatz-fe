643a0e7b3625f3fdef45d9d8a2daad1f19391d9b
Devynne Marshall <devynnem@gmail.com>
2023-07-14 21:14:32 -0600

    feat:Nearly finsih testing for truck details spec

--- cypress/e2e/truck_detailsspec.cy.js
+++ cypress/e2e/truck_detailsspec.cy.js
@@ -15,7 +15,7 @@ describe('Single Truck Page', () => {
     })
     cy.intercept("GET", "https://streeteatz-be-b15261620498.herokuapp.com/items/", {
       statusCode: 200,
-      fixture: "menuData.json"
+      fixture: "ramenMenu.json"
     })
     cy.visit("http://localhost:3000/");
     cy.wait(2000);
@@ -23,29 +23,7 @@ describe('Single Truck Page', () => {
       .get('.name').contains('Mac and Noodles').click();
   });
 
-  it('should see the name, address, tags, and wait time of the truck', () => {
-    cy.get('.details-name').contains('h2', 'Ninja Ramen')
-      .get('.details-status-container').contains('p', '100 Broadway St Denver, CO 80203')
-      .get('.details-tags').contains('p', '#ramen #soup #noodles #asian')
-      .get('.wait-time').contains('p', '10 minutes wait')
-      .get('.details-image')
-  });
-
-  it.skip('should see the likes and dislikes', () => {
-    cy.get('.right-side')
-      .get('.votes-container').contains('p', '56')
-      .get('.details-icon').get('.downvoted')
-  });
-
-  it.skip('should see a menu', () => {
-    cy.get('.menu-container')
-      // .get('.menu-item').contains('h3', 'Vegan Samosas')
-      // .get('.menu-top').contains('p', '$5.00')
-      // .get('.menu-item').contains('h3', 'Gulab Jamun')
-      // .get('.menu-bottom').contains('p', 'Milk doughnuts soaked in syrup')
-  });
-
-  it.skip('should have a logo and be able to go back to main page', () => {
+  it('should have a logo and be able to go back to main page', () => {
     cy.get('.header-container')
       .get('.left-container')
       .get('.logo-name').contains('p', 'street eatz').click()
@@ -56,14 +34,33 @@ describe('Single Truck Page', () => {
     cy.get('.toggle-container')
       .get('button').get('.selected-btn').click()
     cy.contains('h2', 'hungry?')
-    cy.get('.toggle-container')
-      .get('button').get('.btn').click()
-    cy.contains('welcome back!')
   });
 
   it('should be able to go to Vendor View from truck details page', () => {
     cy.get('.toggle-container')
       .get('button').get('.btn').click()
     cy.contains('welcome back!')
+  });
+
+  it('should see the name, address, tags, and wait time of the truck', () => {
+    cy.get('.details-name').contains('h2', 'Ninja Ramen')
+      .get('.details-status-container').contains('p', '100 Broadway St Denver, CO 80203')
+      .get('.details-tags').contains('p', '#ramen #soup #noodles #asian')
+      .get('.wait-time').contains('p', '10 minutes wait')
+      .get('.details-image')
+  });
+
+  it('should see the likes and dislikes', () => {
+    cy.get('.right-side')
+      .get('.votes-container').contains('p', '56')
+      .get('.details-icon').get('.downvoted')
+  });
+
+  it('should see a menu', () => {
+    cy.get('.menu-container')
+      // .get('.menu-item').contains('h3', 'Vegan Samosas')
+      // .get('.menu-top').contains('p', '$5.00')
+      // .get('.menu-item').contains('h3', 'Gulab Jamun')
+      // .get('.menu-bottom').contains('p', 'Milk doughnuts soaked in syrup')
   });
 });
